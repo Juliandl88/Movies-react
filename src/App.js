b77f6b35f0218a289004fc30beb92e5272b7ac9a
Julian <julian_delisio@hotmail.com>
2022-05-10 23:45:38 -0300

    finalizando

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {MoviesGrid} from "./MoviesGrid";
 export function App() {
   return <div className="App">
     <header>
-      <h1 className="title">Películas</h1>
+      <h1 className="title">AppMovies</h1>
     </header>
     <main>
       <MoviesGrid />
