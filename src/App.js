25435830f3216edb1dc171efb1185eb0b48b3028
Julian <julian_delisio@hotmail.com>
2022-05-11 17:16:15 -0300

    creando rutas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import "./App.css";
-import {MoviesGrid} from "./MoviesGrid";
+import { MoviesGrid } from "./components/MoviesGrid";
 
 export function App() {
-  return <div className="App">
-    <header>
-      <h1 className="title">AppMovies</h1>
-    </header>
-    <main>
-      <MoviesGrid />
-    </main>
-  </div>;
+  return (
+    <div className="App">
+      <header>
+        <h1 className="title">Lista de películas</h1>
+      </header>
+      <main>
+        <MoviesGrid />
+      </main>
+    </div>
+  );
 }
-
-
