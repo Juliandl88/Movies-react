b97a3a53d2ac90086a26d60c10c0cad79ede7839
Julian <julian_delisio@hotmail.com>
2023-06-12 22:47:51 -0300

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export function App() {
     <Router>
       <header>
         <Link to="/">
-          <h1 className={styles.title}>Peliculas - Movies</h1>
+          <h1 className={styles.title}>Peliculas</h1>
         </Link>
       </header>
       <main>
